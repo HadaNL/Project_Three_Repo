3baf93bd7b4454853183b0871aa7df5e76d31bf5
HadaNL <119675498+HadaNL@users.noreply.github.com>
2026-04-30 16:37:03 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,317 @@
+
+let gameData = {
+  cnvW: 900,
+  cnvH: 500,
+
+  img_PosX: 0,
+  img_PosY: 0,
+
+  cnvColor: 126,
+  scene: 0,
+  playerOne_Selector: null,
+  playerTwo_Selector: null,
+  p1Color: null,
+  p2Color: null,
+
+  p1_raceAvatar: null,
+  p2_raceAvatar: null,
+  stageSelector: null,
+
+  img_Container: []
+}
+
+function preload() {
+  gameData.character_Selection_Container = loadImage("assets/character_images/character_Sheet_01.png");
+
+  gameData.img_Container[0] = loadImage("assets/background_Images/track_field_Stage.jpg");
+
+  gameData.img_Container[1] = loadImage("assets/background_Images/street_Stage.jpg");
+
+  gameData.img_Container[2] = loadImage("assets/background_Images/city_Stage_on.jpg");
+
+  gameData.img_Container[3] = loadImage("assets/background_Images/city_Stage_off.jpg");
+
+  gameData.img_Container[4] = loadImage("assets/background_Images/bw_track_field_Stage.jpg");
+
+  gameData.img_Container[5] = loadImage("assets/background_Images/bw_street_Stage.jpg");
+    
+  gameData.img_Container[6] = loadImage("assets/background_Images/bw_city_Stage_on.jpg");
+
+  gameData.img_Container[7] = loadImage("assets/background_Images/bw_city_Stage_off.jpg");  
+}
+
+function setup() {
+  createCanvas(gameData.cnvW,gameData.cnvH);
+  background(gameData.cnvColor);
+}
+
+function draw() {
+  switch(gameData.scene) {
+    case 0:
+      characterSelect();
+      break;
+    case 1:
+      raceLvl();
+      break;  
+    default: 
+  }
+}
+
+function keyPressed() {
+  if(gameData.scene === 0) {
+ 
+    if(gameData.playerOne_Selector.player_ready && gameData.playerTwo_Selector.player_ready ) {
+     return ;
+    }
+
+    if(key === 'w' && !gameData.playerOne_Selector.player_ready && gameData.playerOne_Selector.avatar_Color !== null) {
+      gameData.playerOne_Selector.player_ready = true;
+    }
+
+    if(key === 's' && !gameData.playerTwo_Selector.player_ready && gameData.playerTwo_Selector.avatar_Color !== null) {
+      gameData.playerTwo_Selector.player_ready = true;
+    }
+
+    if(key === 'a' && gameData.playerOne_Selector.indicator_position === 0){
+      gameData.playerOne_Selector.indicator_position++;  
+    } else if (key === 'a' && gameData.playerOne_Selector.indicator_position === 1) {
+      gameData.playerOne_Selector.indicator_position++;
+    } else if (key === 'a' && gameData.playerOne_Selector.indicator_position === 2) {
+      gameData.playerOne_Selector.indicator_position++;
+    } else if (key === 'a' && gameData.playerOne_Selector.indicator_position === 3) {
+      gameData.playerOne_Selector.indicator_position++;
+    } else if (key === 'a' && gameData.playerOne_Selector.indicator_position === 4) {
+      gameData.playerOne_Selector.indicator_position = 1;
+    } 
+
+    if(key === 'd' && gameData.playerTwo_Selector.indicator_position === 0){
+      gameData.playerTwo_Selector.indicator_position++;
+    } else if (key === 'd' && gameData.playerTwo_Selector.indicator_position === 1) {
+      gameData.playerTwo_Selector.indicator_position++;
+    } else if (key === 'd' && gameData.playerTwo_Selector.indicator_position === 2) {
+      gameData.playerTwo_Selector.indicator_position++;
+    } else if (key === 'd' && gameData.playerTwo_Selector.indicator_position === 3) {
+      gameData.playerTwo_Selector.indicator_position++;
+    } else if (key === 'd' && gameData.playerTwo_Selector.indicator_position === 4) {
+      gameData.playerTwo_Selector.indicator_position = 1;
+    } 
+  }
+}
+
+class Selection_Indicator {
+  constructor(tS,pNo,x1,y1,x2,y2,sO) {
+    this.tSize = tS;
+    this.player_No = pNo;
+
+    this.pos_X1 = x1;
+    this.pos_Y1 = y1;
+
+    this.pos_X2 = x2;
+    this.pos_Y2 = y2;
+
+    this.opacity = sO;
+
+    this.r = random(255);
+    this.g = random(255);
+    this.b = random(255);
+
+    this.sW = 5;
+    
+    this.tColor = 255;
+
+    this.indicator_position = 0;
+
+    this.player_ready = false;
+
+    this.avatar_Color = null;
+  }
+
+  displayIndicator() {
+    let textPosX;
+    let textPosY;
+
+    let offset_Y = 30;
+
+    let Indicator_Center = (this.pos_X2 - this.pos_X1) / 2;
+    textPosX = this.pos_X1 + Indicator_Center;
+    textPosY;
+
+    if(this.indicator_position !== 0) {
+      this.opacity = 255;
+    }
+    
+    switch(this.indicator_position) {
+      case 1:
+        if(this.player_No === 1) {
+          this.avatar_Color = 'orange';
+          this.pos_X1 = 45;
+          this.pos_Y1 = 134;
+          this.pos_X2 = 151;
+          this.pos_Y2 = 134;
+          textPosY = this.pos_Y1 - offset_Y;
+        } else {
+          this.avatar_Color = 'orange';
+          this.pos_X1 = 45;
+          this.pos_Y1 = 364;
+          this.pos_X2 = 151;
+          this.pos_Y2 = 364;
+          textPosY = this.pos_Y1 + offset_Y;
+        }
+        break;
+      case 2:
+        if(this.player_No === 1) {
+          this.avatar_Color = 'red';
+          this.pos_X1 = 265;
+          this.pos_Y1 = 134;
+          this.pos_X2 = 371;
+          this.pos_Y2 = 134;
+          textPosY = this.pos_Y1 - offset_Y;
+        } else {
+          this.avatar_Color = 'red';
+          this.pos_X1 = 265;
+          this.pos_Y1 = 364;
+          this.pos_X2 = 371;
+          this.pos_Y2 = 364;
+          textPosY = this.pos_Y1 + offset_Y;
+        }
+        break;
+      case 3:
+        if(this.player_No === 1) {
+          this.avatar_Color = 'pink';
+          this.pos_X1 = 519;
+          this.pos_Y1 = 134;
+          this.pos_X2 = 625;
+          this.pos_Y2 = 134;
+          textPosY = this.pos_Y1 - offset_Y;
+        } else {
+          this.avatar_Color = 'pink';
+          this.pos_X1 = 519;
+          this.pos_Y1 = 364;
+          this.pos_X2 = 625;
+          this.pos_Y2 = 364;
+          textPosY = this.pos_Y1 + offset_Y;
+        }
+        break;
+      case 4:
+          if(this.player_No === 1) {
+          this.avatar_Color = 'blue';
+          this.pos_X1 = 750;
+          this.pos_Y1 = 134;
+          this.pos_X2 = 856;
+          this.pos_Y2 = 134;
+          textPosY = this.pos_Y1 - offset_Y;
+        } else {
+          this.avatar_Color = 'blue';
+          this.pos_X1 = 750;
+          this.pos_Y1 = 364;
+          this.pos_X2 = 856;
+          this.pos_Y2 = 364;
+          textPosY = this.pos_Y1 + offset_Y;
+        }
+        break;  
+      default:
+        this.opacity = 0;
+    }
+
+
+    push();
+    fill(this.tColor);
+    textAlign(CENTER, CENTER);
+    textSize(this.tSize);
+    text("P" + this.player_No, textPosX, textPosY);
+    pop();
+
+    push();
+    strokeWeight(this.sW);
+    stroke(this.r,this.g,this.b,this.opacity);
+    line(this.pos_X1,this.pos_Y1,this.pos_X2,this.pos_Y2);
+    pop();
+
+    if(this.player_No === 1) {
+      if(this.player_ready) {
+        push();
+        fill(this.tColor);
+        textAlign(CENTER,CENTER);
+        textSize(this.tSize);
+        text( 'P'+ this.player_No + ' Ready', 200, 450);
+        pop();
+      }
+    } else {
+      if(this.player_ready) {
+        push();
+        fill(this.tColor);
+        textAlign(CENTER,CENTER);
+        textSize(this.tSize);
+        text( 'P'+ this.player_No + ' Ready', 700, 450);
+        pop();
+      }
+    }
+    return this.avatar_Color;   
+  }
+}
+
+class Avatar {
+  constructor(aC) {
+    this.avatar_C = aC;
+
+    this.avatar_X = null;
+    this.avatar_Y = null;
+
+    if(gameData.stageSelector === 0) {
+      
+    }
+  }
+
+  show() {
+    
+  }
+}
+
+function characterSelect() {
+  background(gameData.cnvColor);
+
+  if(gameData.playerOne_Selector === null && gameData.playerTwo_Selector === null) {
+    gameData.playerOne_Selector = new Selection_Indicator(30,1,0,0,0,0,255);
+
+    gameData.playerTwo_Selector = new Selection_Indicator(30,2,0,0,0,0,255);
+  }
+
+  let sprite_Sheet_H = gameData.character_Selection_Container.height;
+  let scaleBy = .2;
+
+  let spriteSize = sprite_Sheet_H * scaleBy;
+
+  let center_Along_y = (gameData.cnvH/2) - (spriteSize / 2);
+
+  image(gameData.character_Selection_Container,0,center_Along_y,spriteSize,spriteSize,0,0,255,255);
+
+  image(gameData.character_Selection_Container,221,center_Along_y,spriteSize,spriteSize,0,256,255,255);
+
+  image(gameData.character_Selection_Container,474,center_Along_y,spriteSize,spriteSize,0,512,255,255);
+
+  image(gameData.character_Selection_Container,702,center_Along_y,spriteSize,spriteSize,0,768,255,255);
+
+  gameData.p1Color = gameData.playerOne_Selector.displayIndicator();
+  gameData.p2Color = gameData.playerTwo_Selector.displayIndicator();
+
+  if (gameData.playerOne_Selector.player_ready && gameData.playerTwo_Selector.player_ready) {
+    gameData.scene = 1;
+  }
+}
+
+function raceLvl() {
+  background(gameData.cnvColor);
+
+  // if(gameData.p1_raceAvatar === null && gameData.p2_raceAvatar === null) {
+  //   gameData.p1_raceAvatar = new Avatar();
+  //   gameData.p2_raceAvatar = new Avatar();
+  //   gameData.stageSelector = floor(random(gameData.img_Container.length));
+  // }
+
+  // image(gameData.img_Container[gameData.stageSelector], gameData.img_PosX,gameData.img_PosY,gameData.cnvW,gameData.cnvH);
+    
+  image(gameData.img_Container[gameData.stageSelector])
+  
+  
+
+}
